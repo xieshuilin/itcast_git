02f26bc7bd9ddffe5ad6ac0100c267f26495b5e7
root <root>
2020-04-21 19:29:16 +0800

    first commit

---
+++ ttt/src/main/java/pojo/user.java
@@ -0,0 +1,28 @@
+package pojo;
+
+/**
+ * @ClassName: user
+ * @Description: TODO
+ * @author: 谢先生
+ * @date: 1998/07/03
+ */
+public class user {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
